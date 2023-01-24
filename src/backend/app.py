fc6a1d63219df3ff390ff696d7b1e3777dcb8d99
Shion Fujimori <shion888@gmail.com>
2023-01-25 00:50:47 +0900

    Add lambda handler

---
+++ src/backend/app.py
@@ -0,0 +1,24 @@
+from http import HTTPStatus
+from aws_lambda_powertools.utilities.typing import LambdaContext
+import json
+
+
+def make_response(statusCode: int, message: str, body: dict = {}) -> dict:
+    return {
+        "statusCode": statusCode,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+        },
+        "message": message,
+        "body": json.dumps(body),
+    }
+
+
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    try:
+        return make_response(HTTPStatus.OK, "Successful", "Hello World")
+    except Exception as e:
+        print(e)
+        return make_response(HTTPStatus.INTERNAL_SERVER_ERROR, "failed to get context")
